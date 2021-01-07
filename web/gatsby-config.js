2efdbd4fa08fdf4df9935285057a0a768350a784
Vishrut Jha <jha.vishrut@gmail.com>
2021-01-07 11:59:45 +0530

    Library updates, Google tracking tags

--- web/gatsby-config.js
+++ web/gatsby-config.js
@@ -19,6 +19,33 @@ module.exports = {
         watchMode: !isProd,
         overlayDrafts: !isProd
       }
-    }
+    },
+    {
+      resolve: 'gatsby-plugin-google-gtag',
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "G-0YD4P88JBG"
+        ],
+        // This object gets passed directly to the gtag config command
+        // This config will be shared across all trackingIds
+        gtagConfig: {
+          optimize_id: "OPT_CONTAINER_ID",
+          anonymize_ip: false,
+          cookie_expires: 0,
+        },
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: true,
+          // Setting this parameter is also optional
+          respectDNT: true,
+          // Avoids sending pageview hits from custom paths
+          exclude: ["/preview/**", "/do-not-track/me/too/"],
+          // localStorage key that has to be set to true for plugin to load
+          optinKey: "gtag_optin", // default
+        },
+      },
+    },
   ]
 }
