27a020a76348fe8a82d557ef20a6af15d4e73b88
Vishrut Jha <jha.vishrut@gmail.com>
2021-02-25 21:26:24 +0530

    Copyrights and Image preview size

--- web/src/components/layout.js
+++ web/src/components/layout.js
@@ -11,10 +11,7 @@ const Layout = ({children, onHideNav, onShowNav, showNav, siteTitle}) => (
     <footer className={styles.footer}>
       <div className={styles.footerWrapper}>
         <div className={styles.siteInfo}>
-          &copy; {new Date().getFullYear()}, Built with <a href='https://www.sanity.io'>Sanity</a>{' '}
-          &amp;
-          {` `}
-          <a href='https://www.gatsbyjs.org'>Gatsby</a>
+          &copy; {new Date().getFullYear()}, Built by <a href='https://vishrutjha.in'>Vishrut Jha</a>
         </div>
       </div>
     </footer>
