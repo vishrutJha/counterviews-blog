35201bc1ae8d42aeff7716c85bc2d38f4d0bed4e
Vishrut Jha <jha.vishrut@gmail.com>
2021-06-10 00:37:00 +0530

    Category Pages in Sanity Studio

--- studio/schemas/documents/category.js
+++ studio/schemas/documents/category.js
@@ -12,6 +12,26 @@ export default {
       name: 'description',
       type: 'text',
       title: 'Description'
-    }
+    },
+    {
+      name: 'mainImage',
+      type: 'mainImage',
+      title: 'Main image'
+    },
+    {
+      name: 'excerpt',
+      type: 'text',
+      title: 'Excerpt',
+      description:
+        'This ends up on summary pages, on Google, when people share this page in social media.'
+    },
+    {
+      name: 'slug',
+      type: 'slug',
+      title: 'Slug',
+      option: {
+        source: 'title'
+      }
+    },
   ]
 }
