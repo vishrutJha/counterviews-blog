f4722eaa2a0e3b377299a14c8b0d8eaef44f609d
Vishrut Jha <vishrut.jha@nighthack.in>
2021-06-10 01:10:31 +0530

    Category Pages in Sanity Studio (#8)
    * Category Pages in Sanity Studio
    * Category Admin tuning
    
    Co-authored-by: Vishrut Jha <jha.vishrut@gmail.com>

--- studio/schemas/documents/category.js
+++ studio/schemas/documents/category.js
@@ -9,8 +9,29 @@ export default {
       title: 'Title'
     },
     {
-      name: 'description',
+      name: 'slug',
+      type: 'slug',
+      title: 'Slug',
+      options: {
+        source: 'title',
+        maxLength: 96
+      }
+    },
+    {
+      name: 'mainImage',
+      type: 'mainImage',
+      title: 'Main image'
+    },
+    {
+      name: 'excerpt',
       type: 'text',
+      title: 'Excerpt',
+      description:
+        'This ends up on summary pages, on Google, when people share this page in social media.'
+    },
+    {
+      name: 'description',
+      type: 'bodyPortableText',
       title: 'Description'
     }
   ]
