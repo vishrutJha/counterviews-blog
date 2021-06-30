1b99334e39589ce6c85d5fd15ab6065cb8568d7b
Vishrut Jha <jha.vishrut@gmail.com>
2021-06-30 23:56:22 +0530

    Changes to production

--- web/client-config.js
+++ web/client-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   sanity: {
     projectId: process.env.GATSBY_SANITY_PROJECT_ID || 'r92ys1n8',
-    dataset: process.env.GATSBY_SANITY_DATASET || 'testing'
+    dataset: process.env.GATSBY_SANITY_DATASET || 'production'
   }
 }
