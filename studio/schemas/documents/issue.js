4ea88ecc7ea00864b074fd05cff2a3858cb9c284
Vishrut Jha <jha.vishrut@gmail.com>
2021-06-30 09:58:32 +0530

    Missed issues template

---
+++ studio/schemas/documents/issue.js
@@ -0,0 +1,50 @@
+export default {
+  name: 'issue',
+  type: 'document',
+  title: 'Issue',
+  fields: [
+    {
+      name: 'title',
+      type: 'string',
+      title: 'Title'
+    },
+    {
+      name: 'slug',
+      type: 'slug',
+      title: 'Slug',
+      option: {
+        source: 'title'
+      }
+    },
+    {
+      name: 'mainImage',
+      type: 'mainImage',
+      title: 'Main image'
+    },
+    {
+      name: 'excerpt',
+      type: 'text',
+      title: 'Excerpt',
+      description:
+        'This ends up on summary pages, on Google, when people share this page in social media.'
+    },
+    {
+      name: 'description',
+      type: 'excerptPortableText',
+      title: 'Description'
+    },
+    {
+      name: 'posts',
+      type: 'array',
+      title: 'Articles',
+      of: [
+        {
+          type: 'reference',
+          to: {
+            type: 'post'
+          }
+        }
+      ]
+    }
+  ]
+}
